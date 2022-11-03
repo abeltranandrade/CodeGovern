a530d5878af73d9fc695fe44af59f1a5c3772917
abeltranandrade <abeltranandrade@smith.edu>
2022-11-03 18:43:10 -0400

    Function is able to download the specific dataset HTML page given a name as argument. Some exploration commented at the bottom.

--- R/get_gov_file.R
+++ R/get_gov_file.R
@@ -1,3 +1,5 @@
+library(rvest)
+library(stringr)
 
 #' Title
 #'
@@ -5,6 +7,37 @@
 #' @export
 #'
 #' @examples
-get_gov_file <- function(){
-  return(1)
+#'
+get_gov_file <- function(name, type){
+  prefix <- "https://catalog.data.gov/dataset"
+  #Specific name needs to be lowercase and with dashes between words
+  specific <- tolower(name)
+  specific <- gsub(" ", "-", specific)
+  # paste the prefix and name together with a slash and no spaces
+  full_url <- paste0(prefix,"/", specific, sep = "")
+  #get HTML of the name given
+  html <- read_html(full_url)
+
+  return(html)
+
 }
+#This below is all just experimenting
+# name <- "Lottery Powerball Winning Numbers Beginning 2010"
+# print(get_gov_file(name))
+#
+# test <- read_html("https://catalog.data.gov/dataset/lottery-powerball-winning-numbers-beginning-2010")
+#
+# sections <- test %>% html_elements("item prop")
+#
+# #. means class
+# tag <- test %>% html_elements(".btn-group")
+#
+# link <- test %>% html_elements("a") %>% html_attr("title")
+#
+# link <- test %>% html_elements("#dataset-resources") %>% html_elements("a")
+#
+# btnlink <- test %>% html_elements("#dataset-resources") %>% html_elements("a") %>% html_elements(".btn btn-primary")
+#
+# btnlink <- test %>% html_elements("#dataset-resources") %>% html_elements(".btn btn-primary")
+#
+# text <- link %>% html_elements(".btn btn-primary")
