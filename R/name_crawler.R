9e0f149bce8837726b8239acf030516539267eb3
abeltranandrade <abeltranandrade@smith.edu>
2022-11-19 01:23:23 -0800

    Working on functionality to be able to pick a certain data type, org type, topic or org. THis is the extremely messy copy. Will clean up now but wanted to keep the current working copy of the code before cleaning up.

--- R/name_crawler.R
+++ R/name_crawler.R
@@ -1,18 +1,24 @@
 
 library(rvest)
-
+library(tidyverse)
+library(stringr)
 library(purrr)
 
 #future parameters could be filter conditions so they can filter by certain area or by type of file
-name_crawler <- function(entries){
+name_crawler <- function(entries, topic = NA, format = NA, OrgType = NA, Org = NA){
   # Bring in the  starting point html
   returnNames <- c()
   #Boolean to know if it has already used the base case
   baseCase <- TRUE
   #This is the URL offset counter, satarts at 2 because we start using it on the second page we scrape
   OffsetCounter <- 2
-  #scraping the root url
+  #Boolean to let us know if to use ? or & to join the parameters
+  FirstParameter <- TRUE
+  FPPrefix <- "?"
+  NFPPrefix <- "&"
   startingPoint <- "https://catalog.data.gov/dataset"
+
+  #scraping the root url
   html <- read_html(startingPoint)
 
   # keep scraping titles if our list to return has less entries than what they asked for
@@ -44,6 +50,12 @@ name_crawler <- function(entries){
 titles <- name_crawler(35)
 titles
 
+
+
+########################################################################################
+# THIS IS TO REMOVE UNWANTED
+########################################################################################
+
 # Function takes off all punctuation from a string in the format we need (periods call for spaces)
 removeSymbol <- function(symbol,stringToChange){
   # Want to do space between periods because we have to make them dashes when turning them to URL
@@ -64,7 +76,7 @@ j
 #removes all the symbols we want from a vector
 removingSymbols <- function(vectorToChange) {
   symbols <- c("!","@","#", "$", "%", "^", "&", "*", "(",")","-","+","=", "{", "}", "|", ".", ",", "?","<",">")
-  symbolsSimplified <- c("&", "*", "\\(","\\)", "\\.", ",", "'", ":")
+  symbolsSimplified <- c("&", "*", "\\(","\\)", "\\.", ",", "'", ":", "-", "/")
   newTitleVector <- c()
 
   for(word in vectorToChange ){
@@ -106,5 +118,162 @@ k
 change2 <- sub("\\.", " ", titles)
 change2[8]
 
+
+#thing that doesnt work I am workingon
+
+#future parameters could be filter conditions so they can filter by certain area or by type of file (leave Untouched)
+name_crawler <- function(entries, topic = NA, format = NA, OrgType = NA, Org = NA){
+  # Bring in the  starting point html
+  returnNames <- c()
+  #Boolean to know if it has already used the base case
+  baseCase <- TRUE
+  #This is the URL offset counter, satarts at 2 because we start using it on the second page we scrape
+  OffsetCounter <- 2
+  #Boolean to let us know if to use ? or & to join the parameters
+  FirstParameter <- TRUE
+  FPPrefix <- "?"
+  NFPPrefix <- "&"
+  startingPoint <- "https://catalog.data.gov/dataset"
+  filter_str <- c("groups=","res_format=","organization_type=","organization=")
+  topic <- NA
+  format <- "csv"
+  OrgType <- NA
+  Org <- NA
+
+  full_url <-""
+  #formatting the URL with correct filters
+  if(!(is.na(topic))){
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[1], topic, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[1], topic, sep ="")}
+  }
+  if(!(is.na(format))){
+    format <- toupper(format) # transformation Needed upper case
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[2], format, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[2], format, sep ="")}
+  }
+  if(!(is.na(OrgType))){
+    OrgType <- gsub(" ", "+",OrgType) # transformation Needed + between words
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[3], OrgType, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[3], OrgType, sep ="")}
+  }
+  if(!(is.na(Org))){ #this one for some reason always uses&
+    Org <- str_to_title(Org)
+    Org <- gsub(" ", "-",Org) # transformation Needed + between words
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[4], Org, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[4], Org, sep ="")}
+  }
+  full_url
+  #scraping the root url
+  html <- read_html(full_url)
+
+  # keep scraping titles if our list to return has less entries than what they asked for
+  while(length(returnNames) < entries){
+    #base case if this is the first page they scrape
+    if(baseCase){
+      # for the current page, get all the link tags and take their text to get the names (maybe include soomethig to take off special characters)
+      aTag <-html  %>% html_nodes('.dataset-heading') %>% html_nodes('a')
+      names <- aTag %>% html_text()
+      returnNames <- append(returnNames, names)
+      baseCase <- FALSE
+    }
+    else{
+      # creating the offset page URL and scraping the HTML
+      pre <- "/?page="
+      print(as.character(OffsetCounter))
+      newURL <- paste0(startingPoint,pre, as.character(OffsetCounter))
+      html <- read_html(newURL)
+      #finding the link tags with the titles
+      aTag <-html  %>% html_nodes('.dataset-heading') %>% html_nodes('a')
+      # here we could divide if its used for the users or for internal testing
+      names <- aTag %>% html_text()
+      returnNames <- append(returnNames, names)
+      OffsetCounter <- OffsetCounter +1
+    }
+  }
+  return(returnNames[0:entries])
+}
+titles <- name_crawler(35)
+titles
+
+
+# function to write the URLs
+creatingFilteredURL <- function(startingPoint, topic = NA, format = NA, OrgType = NA, Org = NA){
+  #Boolean to let us know if to use ? or & to join the parameters
+  FirstParameter <- TRUE
+  FPPrefix <- "?"
+  NFPPrefix <- "&"
+  filter_str <- c("groups=","res_format=","organization_type=","organization=")
+
+  full_url <-""
+  #formatting the URL with correct filters
+  if(!(is.na(topic))){
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[1], topic, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[1], topic, sep ="")}
+  }
+  if(!(is.na(format))){
+    format <- toupper(format) # transformation Needed upper case
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[2], format, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[2], format, sep ="")}
+  }
+  if(!(is.na(OrgType))){
+    OrgType <- gsub(" ", "+",OrgType) # transformation Needed + between words
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[3], OrgType, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[3], OrgType, sep ="")}
+  }
+  if(!(is.na(Org))){ #this one for some reason always uses&
+    Org <- str_to_title(Org)
+    Org <- gsub(" ", "-",Org) # transformation Needed + between words
+    if(FirstParameter){full_url <- paste(startingPoint, FPPrefix, filter_str[4], Org, sep =""); FirstParameter <- FALSE}
+    else{full_url <- paste(full_url, NFPPrefix, filter_str[4], Org, sep ="")}
+  }
+  return(full_url)
+}
+
+name_crawler2 <- function(entries, topic = NA, format = NA, OrgType = NA, Org = NA){
+  # Bring in the  starting point html
+  returnNames <- c()
+  #Boolean to know if it has already used the base case
+  baseCase <- TRUE
+  #This is the URL offset counter, satarts at 2 because we start using it on the second page we scrape
+  OffsetCounter <- 2
+  startingPoint <- "https://catalog.data.gov/dataset"
+  full_url <- creatingFilteredURL(startingPoint ,format = "csv")
+
+  cat(full_url)
+  #scraping the root url
+  html <- read_html(full_url)
+
+  # keep scraping titles if our list to return has less entries than what they asked for
+  while(length(returnNames) < entries){
+    #base case if this is the first page they scrape
+    if(baseCase){
+      # for the current page, get all the link tags and take their text to get the names (maybe include soomethig to take off special characters)
+      aTag <-html  %>% html_nodes('.dataset-heading') %>% html_nodes('a')
+      names <- aTag %>% html_text()
+      returnNames <- append(returnNames, names)
+      baseCase <- FALSE
+    }
+    else{
+      # creating the offset page URL and scraping the HTML
+      pre <- "/?page="
+      print(as.character(OffsetCounter))
+      newURL <- paste0(startingPoint,pre, as.character(OffsetCounter))
+      html <- read_html(newURL)
+      #finding the link tags with the titles
+      aTag <-html  %>% html_nodes('.dataset-heading') %>% html_nodes('a')
+      # here we could divide if its used for the users or for internal testing
+      names <- aTag %>% html_text()
+      returnNames <- append(returnNames, names)
+      OffsetCounter <- OffsetCounter +1
+    }
+  }
+  return(returnNames[0:entries])
+}
+titles2 <- name_crawler2(35)
+titles2
+
+new2 <- removingSymbols(titles)
+
 #Getting all the links(dont think its necessary)
 urls <- html_attr(aTag,'href')
+
