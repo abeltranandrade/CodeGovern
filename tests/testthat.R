81d687d60a118f0c211690cab883c133ba55c7cd
Vibha Gogu <vibhagogu@gmail.com>
2022-11-19 13:43:49 -0500

    test functions

--- tests/testthat.R
+++ tests/testthat.R
@@ -10,3 +10,14 @@ library(testthat)
 library(CodeGovern)
 
 test_check("CodeGovern")
+
+my_Data <- get_gov_file("Lottery Powerball Winning Numbers Beginning 2010", "csv")
+expect_equal(my_Data, read_csv("Lottery Powerball Winning Numbers Beginning 2010.csv"))
+
+my_Data_json <- get_gov_file("Lottery Powerball Winning Numbers Beginning 2010", "json")
+expect_equal(my_Data_json, fromJSON('Lottery Powerball Winning Numbers Beginning 2010.json'))
+
+#expect_error(get_gov_file("123xyz"), 'There was an error. The dataset was not found Error in open.connection(x, "rb") : HTTP error 404.', fixed=TRUE)
+
+
+
