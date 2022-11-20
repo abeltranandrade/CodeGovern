6a3609e99214009cddf9bc720691b1614a5e09d5
Vibha Gogu <vibhagogu@gmail.com>
2022-11-19 22:17:00 -0500

    tests

---
+++ tests/testthat/test-download_json.R
@@ -0,0 +1,14 @@
+require(xml2)
+require(rvest)
+require(stringr)
+require(htmltools)
+require(tidyverse)
+require(readr)
+require(rjson)
+require(methods)
+require(XML)
+my_Data_json <- get_gov_file("Lottery Powerball Winning Numbers Beginning 2010", "json")
+
+test_that("json_downloads", {
+  expect_equal(my_Data_json, fromJSON(file = "Lottery Powerball Winning Numbers Beginning 2010.json"))
+})
